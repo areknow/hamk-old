dcc2b8ab92e1f9b3dae09eb111484650fa0384c4
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-11-05 13:41:11 -0500

    tab colors and icon

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabsComponent } from './tabs/tabs.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [TabsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [TabsComponent]
 })
 export class CoreModule {}
