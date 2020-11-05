d0aa0a562a7e79c771c0fa97e5ca57d439d911db
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-11-04 21:57:11 -0500

    basic tab setup with map

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,17 +4,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    Tab1PageRoutingModule
-  ],
+  imports: [IonicModule, CommonModule, FormsModule, ExploreContainerComponentModule, Tab1PageRoutingModule],
   declarations: [Tab1Page]
 })
 export class Tab1PageModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Keyboard } from '@ionic-native/keyboard';
+declare var require: any;
+const loadGoogleMapsApi = require('load-google-maps-api');
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild('mapTarget', { static: false })
+  set map(target: ElementRef) {
+    if (target) {
+      this.loadMap(target);
+    }
+  }
 
-  constructor() {}
+  searchValue = '';
 
+  async loadMap(target: ElementRef) {
+    try {
+      const googleMaps = await loadGoogleMapsApi({
+        key: environment.mapsApiKey
+      });
+      // tslint:disable-next-line: no-unused-expression
+      new googleMaps.Map(target.nativeElement, {
+        center: {
+          lat: 43.948601,
+          lng: -84.60767
+        },
+        zoom: 7,
+        disableDefaultUI: true
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  submit(x) {
+    console.log(x);
+    Keyboard.hide();
+  }
 }
