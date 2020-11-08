3a013bca3609f42ba7b61eb80b343f49bba8b81e
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-11-08 15:24:06 -0500

    dynamic tab from config

--- src/app/core/tabs/tabs.component.ts
+++ src/app/core/tabs/tabs.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { TABS } from 'src/app/shared/config/tabs';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.scss'],
+  styleUrls: ['./tabs.component.scss']
 })
 export class TabsComponent implements OnInit {
+  tabs = TABS;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
---
+++ src/app/shared/config/tabs.ts
@@ -0,0 +1,32 @@
+export const TABS = [
+  {
+    link: 'map',
+    label: 'Map',
+    icon: 'map',
+    visible: true
+  },
+  {
+    link: 'tab2',
+    label: 'List',
+    icon: 'list',
+    visible: true
+  },
+  {
+    link: 'tab3',
+    label: 'New',
+    icon: 'add_box',
+    visible: true
+  },
+  {
+    link: 'tab4',
+    label: 'Knowledge',
+    icon: 'nature_people',
+    visible: true
+  },
+  {
+    link: 'tab5',
+    label: 'Profile',
+    icon: 'person',
+    visible: true
+  }
+];
