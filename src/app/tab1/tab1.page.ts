17c955cad1f4bbcb8839af2056016a3571936c51
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-11-08 14:12:14 -0500

    better map and geolocation

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,24 +1,27 @@
+/// <reference types="googlemaps" />
+
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Keyboard } from '@ionic-native/keyboard';
-declare var require: any;
-const loadGoogleMapsApi = require('load-google-maps-api');
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Loader } from '@googlemaps/js-api-loader';
+
+interface IButtonGroup {
+  active: boolean;
+  value: number;
+  label: string;
+}
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
-  @ViewChild('mapTarget', { static: false })
-  set map(target: ElementRef) {
-    if (target) {
-      this.loadMap(target);
-    }
-  }
+export class Tab1Page implements OnInit {
+  @ViewChild('mapTarget', { static: false }) mapTarget: ElementRef;
 
   searchValue = '';
-  sheetOpen = true;
+  sheetOpen = false;
 
   resultGroup = [
     {
@@ -55,23 +58,45 @@ export class Tab1Page {
     }
   ];
 
-  async loadMap(target: ElementRef) {
-    try {
-      const googleMaps = await loadGoogleMapsApi({
-        key: environment.mapsApiKey
-      });
-      // tslint:disable-next-line: no-unused-expression
-      new googleMaps.Map(target.nativeElement, {
-        center: {
-          lat: 43.948601,
-          lng: -84.60767
-        },
-        zoom: 7,
+  defaultPosition = {
+    lat: undefined,
+    lng: undefined
+  };
+
+  mapObject: google.maps.Map;
+
+  constructor(private geolocation: Geolocation) {}
+
+  ngOnInit() {
+    this.getLocation();
+  }
+
+  getLocation() {
+    setTimeout(async () => {
+      const {
+        coords: { latitude, longitude }
+      } = await this.geolocation.getCurrentPosition();
+      this.defaultPosition = {
+        lat: latitude,
+        lng: longitude
+      };
+      this.loadMap();
+    }, 100); // time out because ios doesn't want to load the geo location right away
+  }
+
+  async loadMap() {
+    const loader = new Loader({
+      apiKey: environment.mapsApiKey,
+      version: 'weekly'
+    });
+
+    loader.load().then(() => {
+      this.mapObject = new google.maps.Map(this.mapTarget.nativeElement, {
+        center: this.defaultPosition,
+        zoom: 8,
         disableDefaultUI: true
       });
-    } catch (error) {
-      console.error(error);
-    }
+    });
   }
 
   submit() {
@@ -82,21 +107,21 @@ export class Tab1Page {
     this.sheetOpen = !this.sheetOpen;
   }
 
-  handleResultButtonGroup(event) {
+  handleResultButtonGroup(event: number) {
     this.toggleButtonGroupStates(this.resultGroup, event);
   }
 
-  handleAccessibilityButtonGroup(event) {
+  handleAccessibilityButtonGroup(event: number) {
     this.toggleButtonGroupStates(this.accessibilityGroup, event);
   }
 
-  toggleButtonGroupStates(group, event) {
+  toggleButtonGroupStates(group: IButtonGroup[], event: number) {
     for (const button of group) {
-      if (button.value === event) {
-        button.active = true;
-      } else {
-        button.active = false;
-      }
+      button.active = button.value === event;
     }
+  }
+
+  centerMap() {
+    this.mapObject.panTo(this.defaultPosition);
   }
 }
